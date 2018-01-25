7d6cd1993dd74f067db3cde0b4cd7870be54d0d7
Carlos Caxaj <caxaj17@gmail.com>
2018-01-25 14:50:37 -0600

    Primer Commit
    Desarrollo de aplicacion Angular5

---
+++ src/app/envios/envios.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// import { EnviosComponent } from './envios.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+  ]
+})
+export class EnviosModule { }
